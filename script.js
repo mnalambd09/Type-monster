f48d1ec83500e982c836d2a10ffb88e149b20d09
mnalambd09 <mnalambd09@gmail.com>
2022-09-14 22:53:27 +0600

    typing started by fixing the START Button bug

--- script.js
+++ script.js
@@ -107,17 +107,17 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
-      clearInterval(startCountdown);
       startTime = new Date().getTime();
+      clearInterval(startCountdown);
     }
     count--;
   }, 1000);
